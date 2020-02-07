029f73247f76b357e6ff850ffe134c1a13a9eccb
Carlos <carlos.gonzalez@sitiouno.com>
2020-02-06 21:59:56 -0400

    ajustes para prueba

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
+const path = require("path");
 
 app.set("port", PORT);
 
-app.use(express.static(__dirname + "/build"));
+app.use(express.static(path.join(__dirname,"build")));
 
+app.get("/webpage", (req, res) => res.sendFile(path.join(__dirname, "public/index.html")))
+
+app.get("/testingroute", (req, res) => res.send("Todo ok"));
 
 app.listen(app.get("port"), err => {
   if(err) {
